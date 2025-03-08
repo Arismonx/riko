808d867f6c904aaab48e4e1451c1619f62a8ee84
STACIA <69411527+staciax@users.noreply.github.com>
2025-03-08 17:07:15 +0700

    Feat: setup database (#2)
    * feat: init prisma
    
    * feat: ignore .env
    
    * feat: add prisma logging
    
    * feat: test prisma get users

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
 import { Elysia } from 'elysia';
 
+import { prisma } from '@/core/db';
+
 export const app = new Elysia({ name: 'chat-bot' })
+
+
+app.get('/test', () => {
+    const users = prisma.user.findMany({
+        skip: 0, // offset
+        take: 10, // limit
+    });
+    return users;
+})
 
 export type App = typeof app;
---
+++ src/core/db.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient({
+    ...(process.env.NODE_ENV === 'development' && {
+        errorFormat: 'pretty',
+        log: [
+            {
+                emit: 'event',
+                level: 'query',
+            },
+            {
+                emit: 'stdout',
+                level: 'error',
+            },
+            {
+                emit: 'stdout',
+                level: 'info',
+            },
+            {
+                emit: 'stdout',
+                level: 'warn',
+            },
+        ],
+    }),
+});
