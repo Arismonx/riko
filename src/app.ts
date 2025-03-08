bdd8997c059c9ab6d780ba41c252d9366a49170d
Arismonx <arismonx@gmail.com>
2025-03-08 17:40:05 +0700

    fix: remove unused cookie import from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import { Elysia } from 'elysia';
 import { user } from './auth/auth';
-import { cookie } from './auth/cookie'
 import { prisma } from '@/core/db';
 export const app = new Elysia({ name: 'chat-bot' })
 
 app
 .get('/',() => 'Hello World')
 .use(user)
-.use(cookie)
 
 app.get('/test', () => {
     const users = prisma.user.findMany({
