e4c7922588976213313a26b3ed1f154e15704d99
Arismonx <arismonx@gmail.com>
2025-03-08 16:46:57 +0700

    feat: implement user authentication with registration and login endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import { Elysia } from 'elysia';
-
+import { user } from './auth/auth';
+import { cookie } from './auth/cookie'
 export const app = new Elysia({ name: 'chat-bot' })
+
+
+app
+.get('/',() => 'Hello World')
+.use(user)
+.use(cookie)
 
 export type App = typeof app;
