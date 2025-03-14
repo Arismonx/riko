4773da4019b748f91639ed1caf638c61906eea6e
STACiA <69411527+staciax@users.noreply.github.com>
2025-03-14 22:00:26 +0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,19 @@
-import pluginJs from "@eslint/js";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import tseslint from "typescript-eslint";
-
+import pluginJs from '@eslint/js';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {
-    plugins: {
-        "simple-import-sort": simpleImportSort,
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    {
+        plugins: {
+            'simple-import-sort': simpleImportSort,
+        },
+        rules: {
+            'simple-import-sort/imports': 'warn',
+            'simple-import-sort/exports': 'warn',
+        },
     },
-    rules: {
-        "simple-import-sort/imports": "warn",
-        "simple-import-sort/exports": "warn",
-    },
-    ignores: ["**/src/components/ui/**"],
-},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
 ];
