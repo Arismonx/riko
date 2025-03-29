78e5c6109946a4a31d6d8c55bfbc40c5ce1c682e
STACIA <69411527+staciax@users.noreply.github.com>
2025-03-29 15:36:01 +0700

    feat: Add auth plugin (#10)
    * feat: add custom error
    
    * feat: add security, update config and implement auth plugin

---
+++ src/plugins/auth.ts
@@ -0,0 +1,40 @@
+import { Elysia } from 'elysia';
+
+import { prisma } from '@/core/db';
+import { security } from '@/core/security';
+import { HTTPError } from '@/errors';
+
+export const auth = new Elysia({ name: 'auth' })
+    .use(security)
+    .derive(async ({ jwt, cookie: { auth } }) => {
+        console.log('auth', auth.value);
+        const jwtPayload = await jwt.verify(auth.value);
+        if (!jwtPayload) {
+            throw new HTTPError({
+                status: 403,
+                message: 'Could not validate credentials',
+            });
+        }
+
+        const userId = jwtPayload.sub;
+        console.log('userId', userId);
+
+        const user = await prisma.user.findUnique({ where: { id: userId } });
+
+        if (!user) {
+            throw new HTTPError({
+                status: 404,
+                message: 'User not found',
+            });
+        }
+
+        if (!user.isActive) {
+            throw new HTTPError({
+                status: 400,
+                message: 'Inactive user',
+            });
+        }
+
+        return { currentUser: user };
+    })
+    .as('plugin');
