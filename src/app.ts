557b394d758fcee0be8912be7fe6a6a2c4c7d12e
STACiA <stacia.dev@gmail.com>
2025-06-19 22:04:22 +0700

    feat: integrate CORS and Swagger middleware for enhanced API functionality

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,33 @@
+import { cors } from '@elysiajs/cors';
+import { swagger } from '@elysiajs/swagger';
 import { Elysia } from 'elysia';
 
 import { apiRouter } from '@/api';
 import { auth } from '@/auth/auth';
+import { env } from '@/core/config';
 
 export const app = new Elysia({ name: 'ai-chat-bot' }) //
+    .use(
+        cors({
+            // TODO: environment variable for CORS origins
+            origin: [],
+            methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+            credentials: true,
+            allowedHeaders: true,
+        }),
+    )
+    .use((app) => {
+        if (env.NODE_ENV !== 'production')
+            return app.use(
+                swagger({
+                    path: '/api/docs',
+                    documentation: {
+                        info: { title: env.PROJECT_NAME, version: '0.0.1' },
+                    },
+                }),
+            );
+        return app;
+    })
     .use(auth)
     .use(apiRouter({ prefix: '/api/v1' }));
 
