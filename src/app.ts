d82fa15e6399b0fde2fae99342289f532e404191
STACiA <stacia.dev@gmail.com>
2025-06-19 22:28:56 +0700

    refactor: streamline CORS middleware implementation in app setup

--- src/app.ts
+++ src/app.ts
@@ -7,21 +7,14 @@ import { auth } from '@/auth/auth';
 import { env } from '@/core/config';
 
 export const app = new Elysia({ name: 'ai-chat-bot' }) //
-    .use((app) => {
-        const ALL_CORS_ORIGINS: string[] = [
-            ...env.BACKEND_CORS_ORIGINS,
-            env.FRONTEND_HOST,
-        ];
-        app.use(
-            cors({
-                origin: ALL_CORS_ORIGINS,
-                methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-                credentials: true,
-                allowedHeaders: true,
-            }),
-        );
-        return app;
-    })
+    .use(
+        cors({
+            origin: [...env.BACKEND_CORS_ORIGINS, env.FRONTEND_HOST],
+            methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+            credentials: true,
+            allowedHeaders: true,
+        }),
+    )
     .use((app) => {
         if (env.NODE_ENV !== 'production')
             return app.use(
