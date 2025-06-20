28fbaa5afc16916d810a6723a61c17c673de9781
STACIA <69411527+staciax@users.noreply.github.com>
2025-06-20 19:11:35 +0700

    feat: add plugin swagger and cors (#15)
    * feat: add @elysiajs/cors, @elysiajs/swagger  dependencies for ElysiaJS
    
    * feat: integrate CORS and Swagger middleware for enhanced API functionality
    
    * feat: add development environment logging for API documentation
    
    * fix: update dependency versions to use tilde for more controlled updates
    
    * fix: replace process.env with env import for environment variable access
    
    * feat: implement CORS origins configuration and add frontend host to environment variables
    
    * fix: rename CORS_ORIGINS to BACKEND_CORS_ORIGINS for clarity in configuration
    
    * fix: clarify comment on default value for BACKEND_CORS_ORIGINS in environment schema
    
    * feat: add PROJECT_VERSION to environment schema with default from package.json
    
    * refactor: streamline CORS middleware implementation in app setup
    
    * fix: use PROJECT_NAME from environment in Elysia app initialization
    
    * fix: update environment variable names from PROJECT_NAME/PROJECT_VERSION to APP_NAME/APP_VERSION for consistency
    
    * fix: update .env.example to improve clarity and organization of environment variables
    
    * fix: update environment check to enable Swagger documentation only in development mode
    
    * fix: correct allowedHeaders configuration in CORS middleware
    
    * fix: correct import path for package.json in config.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,35 @@
+import { cors } from '@elysiajs/cors';
+import { swagger } from '@elysiajs/swagger';
 import { Elysia } from 'elysia';
 
 import { apiRouter } from '@/api';
 import { auth } from '@/auth/auth';
+import { env } from '@/core/config';
 
-export const app = new Elysia({ name: 'ai-chat-bot' }) //
+export const app = new Elysia({ name: env.APP_NAME }) //
+    .use(
+        cors({
+            origin: [...env.BACKEND_CORS_ORIGINS, env.FRONTEND_HOST],
+            methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+            credentials: true,
+            allowedHeaders: ['Content-Type', 'Authorization'],
+        }),
+    )
+    .use((app) => {
+        if (env.NODE_ENV === 'development')
+            return app.use(
+                swagger({
+                    path: '/api/docs',
+                    documentation: {
+                        info: {
+                            title: env.APP_NAME,
+                            version: env.APP_VERSION,
+                        },
+                    },
+                }),
+            );
+        return app;
+    })
     .use(auth)
     .use(apiRouter({ prefix: '/api/v1' }));
 
